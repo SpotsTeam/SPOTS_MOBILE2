ab7b61548fbb80cecf89a8219c605bccb3bdd3cc
nathanDmoore <Nathan Moore@NathanMoore-PC.(none)>
2015-05-09 08:29:20 -0500

    This does not work... yet?

---
+++ src/app/AppConfig.java
@@ -0,0 +1,10 @@
+package app;
+
+public class AppConfig {
+	// Server user login url
+    public static String URL_LOGIN = "http://52.10.141.9/SPOTS/";
+ 
+    // Server user register url
+    public static String URL_REGISTER = "http://52.10.141.9/SPOTS/";
+
+}
